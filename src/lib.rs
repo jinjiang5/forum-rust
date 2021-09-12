c33b541881387c8e5b9bcba9c975209376c998d1
Zhu Wenjin <Zhu.Wenjin@aliyun.com>
2021-09-12 10:55:15 +0800

    添加sqlite3模块

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub mod config {
+    use r2d2_sqlite::SqliteConnectionManager;
     pub struct MyConfig {
-        pub server_address:String,
-        pub server_port:u16,
+        pub server_address: String,
+        pub server_port: u16,
+        pub sqlite_manager: SqliteConnectionManager,
     }
     impl MyConfig {
         pub fn new() -> Self {
@@ -9,16 +11,42 @@ pub mod config {
             settings.merge(config::File::with_name("Settings")).unwrap();
             let server_address: String = settings.get("server.address").unwrap();
             let server_port: u16 = settings.get("server.port").unwrap();
+            let sqlite_path: String = settings.get("db.sqlite_path").unwrap();
+            let sqlite_manager = SqliteConnectionManager::file(sqlite_path);
             Self {
                 server_address,
-                server_port
+                server_port,
+                sqlite_manager,
             }
         }
     }
 }
+
+pub mod db {
+    use r2d2::PooledConnection;
+    use r2d2_sqlite::SqliteConnectionManager;
+    use r2d2_sqlite::rusqlite::Error;
+    pub fn useradd(conn: PooledConnection<SqliteConnectionManager>, id: u32, name: String) -> Result<usize,Error> {
+        conn.execute(
+            "INSERT INTO users (id, name) VALUES ($1, $2)",
+            [id.to_string(), name],
+        )
+    }
+}
 pub mod headers {
-    use actix_web::HttpResponse;
-    pub fn index() -> HttpResponse {
+    use actix_web::{web, HttpResponse};
+    use r2d2::Pool;
+    use r2d2_sqlite::SqliteConnectionManager;
+
+    use crate::db;
+    pub async fn index() -> HttpResponse {
         HttpResponse::Ok().body("Hello world!")
+    }
+    pub async fn testdb(pool: web::Data<Pool<SqliteConnectionManager>>) -> HttpResponse {
+        let conn = pool.get().unwrap();
+        web::block(|| {
+            db::useradd(conn, 1, "hello".to_string())
+        }).await.unwrap();
+        HttpResponse::Ok().into()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
-use actix_web::{App, HttpServer, middleware, web};
-use forum_rust::headers;
+use actix_web::{middleware, web, App, HttpServer};
 use forum_rust::config::MyConfig as Config;
+use forum_rust::headers;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
     let config = Config::new();
-    HttpServer::new(|| {
+    let pool = r2d2::Pool::new(config.sqlite_manager).unwrap();
+    HttpServer::new(move || {
         App::new()
+            .data(pool.clone())
             .wrap(middleware::Logger::default())
             .route("/", web::get().to(headers::index))
+            .route("/testdb", web::get().to(headers::testdb))
     })
     .bind((config.server_address, config.server_port))?
     .run()
