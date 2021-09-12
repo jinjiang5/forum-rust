d3ac1cb01cee504f905c8b302010d4eb1f3c5587
Zhu Wenjin <Zhu.Wenjin@aliyun.com>
2021-09-12 10:19:39 +0800

    添加Config模块

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+pub mod config {
+    pub struct MyConfig {
+        pub server_address:String,
+        pub server_port:u16,
+    }
+    impl MyConfig {
+        pub fn new() -> Self {
+            let mut settings = config::Config::default();
+            settings.merge(config::File::with_name("Settings")).unwrap();
+            let server_address: String = settings.get("server.address").unwrap();
+            let server_port: u16 = settings.get("server.port").unwrap();
+            Self {
+                server_address,
+                server_port
+            }
+        }
+    }
+}
 pub mod headers {
     use actix_web::HttpResponse;
     pub fn index() -> HttpResponse {
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use actix_web::{App, HttpServer, middleware, web};
 use forum_rust::headers;
+use forum_rust::config::MyConfig as Config;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
+    let config = Config::new();
     HttpServer::new(|| {
         App::new()
             .wrap(middleware::Logger::default())
             .route("/", web::get().to(headers::index))
     })
-    .bind("0.0.0.0:8080")?
+    .bind((config.server_address, config.server_port))?
     .run()
     .await
 }
